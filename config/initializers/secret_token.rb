7a533ab1ce1c9fc19ddad68c143815d9d6c64000
Waley Chen <nikeelevet@hotmail.com>
2013-01-18 21:31:00 -0500

    generate rails scaffold

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MoustacheBurrito::Application.config.secret_token = '6f436ff248d625ca45fae4432b4884fb2617de080c6e32dc875336a4f412377f0f2ef2f097eb25d7b2ac456d026a67aaa6ab59c5efccc0c568d2250b5b652260'
