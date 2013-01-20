6590530015397d74e8d7742b4276fb1e5c8b10f2
Drew Gross <drew.a.gross@gmail.com>
2013-01-20 00:52:00 -0500

    things;

--- app/assets/javascripts/setup.js
+++ app/assets/javascripts/setup.js
@@ -13,17 +13,18 @@ $(function(){
   MoustacheBurrito.end = MoustacheBurrito.time.now() + MoustacheBurrito.time.days(100);
   MoustacheBurrito.secPerPixel = 100;
 
-  var ret = MoustacheBurrito.schedule(MoustacheBurrito.user.tasks, MoustacheBurrito.user.tags);
-  MoustacheBurrito.user.tasks = ret.scheduled;
+  $.get("/get_tasks?email=" + MoustacheBurrito.userid, function (data) {
+    MoustacheBurrito.user.tasks = MoustacheBurrito.schedule(MoustacheBurrito.user.tasks.concat(data), MoustacheBurrito.user.tags).scheduled;
+    console.log(MoustacheBurrito.user.tasks);
+    var view = new MoustacheBurrito.ScheduleView({
+      el: $('body'),
+      start: MoustacheBurrito.start,
+      end: MoustacheBurrito.end,
+      tasks: MoustacheBurrito.user.tasks
+    });
 
-  var view = new MoustacheBurrito.ScheduleView({
-    el: $('body'),
-    start: MoustacheBurrito.start,
-    end: MoustacheBurrito.end,
-    tasks: MoustacheBurrito.user.tasks
+    view.render();
+    view.scrollToTime(MoustacheBurrito.time.now());
+    view.calendarView.scrollToTime(MoustacheBurrito.time.now());
   });
-
-  view.render();
-  view.scrollToTime(MoustacheBurrito.time.now());
-  view.calendarView.scrollToTime(MoustacheBurrito.time.now());
 });
